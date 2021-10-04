5c20017392b621e87aebbacffdbdc563f359c095
Prabhvir Babra <theprabab@gmail.com>
2021-10-03 20:55:33 -0700

    Problem 1

--- 2018-2019/2nd/Silver/problem1.cpp
+++ 2018-2019/2nd/Silver/problem1.cpp
@@ -1,3 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-// Problem Name: s
+// Problem Name: Grass Planting
+
+int main(){
+	
+	freopen("planting.in", "r", stdin);
+	freopen("planting.out", "w", stdout);
+	
+	ios_base::sync_with_stdio(false);
+	
+	int n;
+	cin >> n;
+	
+	int d[n];
+	memset(d, 0, sizeof(d));
+	
+	for(int i = 1; i < n; ++i){
+		int a, b;
+		cin >> a >> b;
+		--a, --b;
+		++d[a], ++d[b];
+	}
+	
+	int ans = 0;
+	for(int& x : d)
+		ans = max(x + 1, ans);
+	
+	cout << ans << "\n";
+	
+	return 0;
+	
+}
